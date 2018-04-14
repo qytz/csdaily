b94cfb4536a9231e3a9fddb57a2f81368549ae8a
qytz <hhhhhf@foxmail.com>
2018-04-14 14:27:34 +0800

    Initial commit

---
+++ csdaily/__version__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# This file is part of csdaily.
+
+# Copyright (C) 2018-present qytz <hhhhhf@foxmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__title__ = 'csdaily'
+__version__ = '0.0.1'
+__description__ = 'China Stock Daily'
+__author__ = 'qytz'
+__author_email__ = 'hhhhhf@foxmail.com'
+__url__ = 'https://github.com/qytz/cn_stock_daily'
+__license__ = 'Apache License 2.0'
---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. China Stock Daily documentation master file, created by
+   sphinx-quickstart on Sat Apr 14 11:42:11 2018.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to China Stock Daily's documentation!
+=============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
